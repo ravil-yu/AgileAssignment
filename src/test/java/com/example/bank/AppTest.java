a44600c091c19de95352681a0fdedc0a5a8bc38f
Yiying YU <a00316106@student.tus.ie>
2024-04-12 12:22:58 +0100

    code modified

--- src/test/java/com/example/bank/AppTest.java
+++ src/test/java/com/example/bank/AppTest.java
@@ -7,29 +7,36 @@ public class AppTest {
 
     @Test
     public void testCalculateBalance() {
-        assertEquals(100.0, App.calculateBalance());
+        assertEquals(100, App.calculateBalance(), 0.001); 
     }
 
     @Test
     public void testCalculateWithdrawSufficientBalance() {
-        assertEquals(10.0, App.calculateWithdraw(10));
+        assertEquals(50, App.calculateWithdraw(50), 0.001); 
     }
 
     @Test
     public void testCalculateWithdrawInsufficientBalance() {
-        assertEquals(0.0, App.calculateWithdraw(110));
-        assertEquals(100.0, App.calculateBalance());
+        assertEquals(0, App.calculateWithdraw(200), 0.001); 
     }
 
     @Test
     public void testCalculateDeposit() {
-        assertEquals(20.0, App.calculateDeposit(20));
+        assertEquals(100, App.calculateDeposit(100), 0.001); 
+    }
+
+    @Test
+    public void testIsBalanceBelow() {
+        assertTrue(App.isBalanceBelow(200)); 
+    }
+
+    @Test
+    public void testIsBalanceNotBelow() {
+        assertFalse(App.isBalanceBelow(50)); 
     }
 
     @Test
     public void testIsBalanceNegative() {
-        assertFalse(App.isBalanceNegative());
+        assertFalse(App.isBalanceNegative()); 
     }
-
-
 }
